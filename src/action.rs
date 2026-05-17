1078b34bcd1bbf05d6578b0ee8666e22b07678d5
Kori <daniilignatev290@proton.me>
2026-05-17 08:28:42 +0300

    Define Action struct with various fields
    Added Action struct with fields for name, category, exec, description, tags, and requires_sudo.

--- src/action.rs
+++ src/action.rs
@@ -1,1 +1,16 @@
+use serde::Deserialize;
 
+#[derive(Debug, Deserialize)]
+pub struct Action {
+    pub name: String,
+    pub category: String,
+    pub exec: String,
+
+    pub description: Option<String>,
+
+    #[serde(default)]
+    pub tags: Vec<String>,
+
+    #[serde(default)]
+    pub requires_sudo: bool,
+}
