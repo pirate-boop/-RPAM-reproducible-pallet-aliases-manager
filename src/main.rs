fb8de6522adbe7b8d2c71bb6376004316b8e45be
Kori <daniilignatev290@proton.me>
2026-05-17 08:32:32 +0300

    Add action and scanner modules to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
+mod action;
+mod scanner;
 
+use std::path::Path;
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let actions = scanner::scan_actions(
+        Path::new("./examples")
+    )?;
+
+    for action in actions {
+        println!("{:#?}", action);
+    }
+
+    Ok(())
+}
